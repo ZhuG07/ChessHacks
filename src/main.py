3e5a48cd7cd61026b9eb99c394e7bed80c431385
Gordon <gordon@gmail.com>
2025-11-16 04:20:32 -0500

    changed to deploy selfplay

--- src/main.py
+++ src/main.py
@@ -14,7 +14,11 @@ import torch
 # ============================================================
 
 # Relative path: src/main.py → src/model_save/best.pt
-MODEL_PATH = Path(r"F:/VS Code Storage/ChessHacks/src/model_save/last.pt")
+
+#MODEL_PATH = Path(r"F:/VS Code Storage/ChessHacks/src/model_save/last.pt")
+#MODEL_PATH = Path(r"F:/VS Code Storage/ChessHacks/src/model_save/last.pt")
+MODEL_PATH = Path(r"F:/VS Code Storage/ChessHacks/src/model_save/best_selfplay.pt")
+
 
 NUM_PLANES = 18
 NUM_PROMOS = 5   # [None, Q, R, B, N]
